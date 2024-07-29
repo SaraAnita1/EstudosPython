eaad65e5b320b5281ee60af0200068d32e555638
Sara Anita <sara.anita.desa@gmail.com>
2024-07-29 16:35:00 -0300

    Initial Commit

---
+++ aula1.py
@@ -0,0 +1,22 @@
+#print('Olá, Mundo !')
+
+#print(Olá mundo)
+
+#print( 7 + 4 )
+
+#print('7' + '4')
+
+#7 + 4
+
+#nome = 'Sara'
+#idade = 27
+#peso = 90
+
+#print(nome,idade,peso)
+#print(nome+ idade + peso) dessa forma aparece erro 
+
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Quantos quilos você pesa?')
+
+print(nome, idade, peso)
---
+++ exercicios aula 1.py
@@ -0,0 +1,20 @@
+#Desafio 1
+
+nome = input ('Qual o seu nome?')
+print('Olá ' + nome + '! Prazer em te conhecer')
+
+#Desafio 2
+
+dia = input('Em qual dia você nasceu?')
+mes = input('Em qual mês você nasceu?')
+ano = input('Em qual ano você nasceu?')
+print('Você nasceu em ' + dia + '/' + mes + '/' + ano + ', Correto?')
+
+#Desafio 3
+
+num1 = input('Por favor insira um numero')
+num2 = input('Insira outro Numero')
+res = int(num1) + int(num2)
+print(f'A soma de {num1} + {num2} é igual a: {res}')
+
+
